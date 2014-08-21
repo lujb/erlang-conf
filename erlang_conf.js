0012b6109fe91683be9359342173570a50762fd5
lujb <lujingboo@gmail.com>
2014-08-21 13:11:53 +0800

    update

--- erlang_conf.js
+++ erlang_conf.js
@@ -152,7 +152,7 @@ module.exports = (function() {
             return {
               'type': 'binstr',
               'length': 1,
-              '0': value
+              '0': value[0]
             }
           },
         peg$c55 = "'",
@@ -166,14 +166,17 @@ module.exports = (function() {
         peg$c61 = { type: "class", value: "[a-z]", description: "[a-z]" },
         peg$c62 = /^[a-zA-Z0-9_@]/,
         peg$c63 = { type: "class", value: "[a-zA-Z0-9_@]", description: "[a-zA-Z0-9_@]" },
-        peg$c64 = "\n",
-        peg$c65 = { type: "literal", value: "\n", description: "\"\\n\"" },
-        peg$c66 = "\r\n",
-        peg$c67 = { type: "literal", value: "\r\n", description: "\"\\r\\n\"" },
-        peg$c68 = /^[ \t]/,
-        peg$c69 = { type: "class", value: "[ \\t]", description: "[ \\t]" },
-        peg$c70 = /^[!"#$%&'()*+,-.\/0-9:;<=>?@A-Z[\\\]\^_`a-z{|}~]/,
-        peg$c71 = { type: "class", value: "[!\"#$%&'()*+,-.\\/0-9:;<=>?@A-Z[\\\\\\]\\^_`a-z{|}~]", description: "[!\"#$%&'()*+,-.\\/0-9:;<=>?@A-Z[\\\\\\]\\^_`a-z{|}~]" },
+        peg$c64 = function(head, tail) {
+            return head.join('')+tail.join('');
+          },
+        peg$c65 = "\n",
+        peg$c66 = { type: "literal", value: "\n", description: "\"\\n\"" },
+        peg$c67 = "\r\n",
+        peg$c68 = { type: "literal", value: "\r\n", description: "\"\\r\\n\"" },
+        peg$c69 = /^[ \t]/,
+        peg$c70 = { type: "class", value: "[ \\t]", description: "[ \\t]" },
+        peg$c71 = /^[!"#$%&'()*+,-.\/0-9:;<=>?@A-Z[\\\]\^_`a-z{|}~]/,
+        peg$c72 = { type: "class", value: "[!\"#$%&'()*+,-.\\/0-9:;<=>?@A-Z[\\\\\\]\\^_`a-z{|}~]", description: "[!\"#$%&'()*+,-.\\/0-9:;<=>?@A-Z[\\\\\\]\\^_`a-z{|}~]" },
 
         peg$currPos          = 0,
         peg$reportedPos      = 0,
@@ -1264,7 +1267,7 @@ module.exports = (function() {
           }
           if (s2 !== peg$FAILED) {
             peg$reportedPos = s0;
-            s1 = peg$c59(s1);
+            s1 = peg$c64(s1, s2);
             s0 = s1;
           } else {
             peg$currPos = s0;
@@ -1294,19 +1297,19 @@ module.exports = (function() {
       var s0;
 
       if (input.charCodeAt(peg$currPos) === 10) {
-        s0 = peg$c64;
+        s0 = peg$c65;
         peg$currPos++;
       } else {
         s0 = peg$FAILED;
-        if (peg$silentFails === 0) { peg$fail(peg$c65); }
+        if (peg$silentFails === 0) { peg$fail(peg$c66); }
       }
       if (s0 === peg$FAILED) {
-        if (input.substr(peg$currPos, 2) === peg$c66) {
-          s0 = peg$c66;
+        if (input.substr(peg$currPos, 2) === peg$c67) {
+          s0 = peg$c67;
           peg$currPos += 2;
         } else {
           s0 = peg$FAILED;
-          if (peg$silentFails === 0) { peg$fail(peg$c67); }
+          if (peg$silentFails === 0) { peg$fail(peg$c68); }
         }
       }
 
@@ -1316,12 +1319,12 @@ module.exports = (function() {
     function peg$parsews() {
       var s0;
 
-      if (peg$c68.test(input.charAt(peg$currPos))) {
+      if (peg$c69.test(input.charAt(peg$currPos))) {
         s0 = input.charAt(peg$currPos);
         peg$currPos++;
       } else {
         s0 = peg$FAILED;
-        if (peg$silentFails === 0) { peg$fail(peg$c69); }
+        if (peg$silentFails === 0) { peg$fail(peg$c70); }
       }
 
       return s0;
@@ -1330,12 +1333,12 @@ module.exports = (function() {
     function peg$parseascii() {
       var s0;
 
-      if (peg$c70.test(input.charAt(peg$currPos))) {
+      if (peg$c71.test(input.charAt(peg$currPos))) {
         s0 = input.charAt(peg$currPos);
         peg$currPos++;
       } else {
         s0 = peg$FAILED;
-        if (peg$silentFails === 0) { peg$fail(peg$c71); }
+        if (peg$silentFails === 0) { peg$fail(peg$c72); }
       }
 
       return s0;
